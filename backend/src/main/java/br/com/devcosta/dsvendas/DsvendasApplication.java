93efcc881d840f36fec97e9bc5bde158ed1b1dc3
Rafael Costa <raf.costa29@gmail.com>
2021-05-03 10:37:34 -0300

    Projetos Criados

---
+++ backend/src/main/java/br/com/devcosta/dsvendas/DsvendasApplication.java
@@ -0,0 +1,13 @@
+package br.com.devcosta.dsvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/devcosta/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devcosta.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
