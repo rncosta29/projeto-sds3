5eab19a19949d7d3ef11a8231e018e1b23647830
Rafael Costa <raf.costa29@gmail.com>
2021-05-07 12:14:34 -0300

    Datatable Integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,3 +1,5 @@
+import { Seller } from './seller';
+
 export type SaleSum = {
     name: string;
     sum: number;
@@ -7,4 +9,25 @@ export type SaleSuccess = {
     sellerName: string;
     visited: number;
     deals: number;
+}
+
+export type Sale = {
+    id: number;
+    visited: number;
+    deals: number;
+    amount: number;
+    date: string;
+    seller: Seller
+}
+
+export type SalePage = {
+    content?: Sale[];
+    last: boolean;
+    totalElements: number;
+    totalPages: number;
+    size?: number;
+    number: number;
+    first: true;
+    numberOfElements?: number;
+    empty?: boolean;
 }
